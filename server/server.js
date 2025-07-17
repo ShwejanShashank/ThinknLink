86077e4f1ace62eccdda9efc1889fb09c9314f77
ShwejanShashank <shwejanshashank.keerthi@ucdenver.edu>
2025-07-17 10:58:24 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,28 +4,28 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-// const path = require("path");//for render.com
+const path = require("path");//for render.com
 var isNextRound = false;
 
 const app = express();
 app.use(cors());
 
 // for render.com
-// app.use(express.static(path.join(__dirname, "../client/build")));
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 
 const server = http.createServer(app);
 
-//const io = new Server(server);//for render.com
-const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-    }
-});
+const io = new Server(server);//for render.com
+// const io = new Server(server, {
+//     cors: {
+//         origin: "http://localhost:3000",
+//         methods: ["GET", "POST"]
+//     }
+// });
 
 const wordList = [
     "Dog", "Car", "Tree", "Book", "Phone", "Table", "Water", "Light",
