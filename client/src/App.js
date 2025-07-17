1a55038d8e23178b09fcd630a551b613d36bcba6
vishnubunny <31568832+vishnubunny@users.noreply.github.com>
2025-07-16 22:00:11 -0600

    Final Version(Colors to be added)

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,73 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+
+
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import Home from "./components/Home";
 import Game from "./components/Game";
 import Results from "./components/Results";
-import FinalScores from "./components/FinalScores";
+
 import { io } from "socket.io-client";
+import FinalScores from "./components/FinalScores";
 
-const socket = io();
 
-const App = () => {
-    return (
-        
-            <Routes>
-                <Route path="/" element={<Home socket={socket} />} />
-                <Route path="/game/:roomId" element={<Game socket={socket} />} />
-                <Route path="/results/:roomId" element={<Results socket={socket} />} />
-                <Route path="/final-scores/:roomId" element={<FinalScores socket={socket} />} />
-            </Routes>
-        
-    );
+const socket = io("http://localhost:5001", {
+    autoConnect: false, // important
+    });
+
+
+
+const AppWrapper = () => {
+
+
+
+
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (performance.navigation.type === 1) {
+      // Reload detected
+      window.location.href = "http://localhost:3000/";
+    } else {
+      if (!socket.connected) {
+        socket.connect();
+      }
+    }
+  }, []);
+
+
+  useEffect(() => {
+    const handleBackButton = (event) => {
+        window.location.href = "/"; 
+    };
+
+    window.history.pushState(null, "", window.location.href);
+  
+    window.addEventListener("popstate", handleBackButton);
+  
+    return () => {
+      window.removeEventListener("popstate", handleBackButton);
+    };
+  }, []);
+
+
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home socket={socket} />} />
+      <Route path="/game/:roomId" element={<Game socket={socket} />} />
+      <Route path="/results/:roomId" element={<Results socket={socket} />} />
+      <Route path="/final-scores/:roomId" element={<FinalScores socket={socket} />} />
+    </Routes>
+  );
 };
 
+const App = () => (
+
+    <AppWrapper />
+
+);
+
 export default App;
-
-
